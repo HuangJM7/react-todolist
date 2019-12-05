680df22fadea11fd6706febad6f8111d19a66d6c
HuangJM7 <jm_william@qq.com>
2019-12-05 23:04:26 +0800

    数据初始化提升到Home组件

---
+++ build/precache-manifest.eb34d9fc6dcb073690208ddc3536c8cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260e1835cb51c8527322f458b5ca26d4",
+    "url": "/react-todolist/build/index.html"
+  },
+  {
+    "revision": "0fdbbbef3336aec576e5",
+    "url": "/react-todolist/build/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "40acf6974ad8ab65cc3d",
+    "url": "/react-todolist/build/static/css/main.e8be195e.chunk.css"
+  },
+  {
+    "revision": "0fdbbbef3336aec576e5",
+    "url": "/react-todolist/build/static/js/2.ac5bae08.chunk.js"
+  },
+  {
+    "revision": "40acf6974ad8ab65cc3d",
+    "url": "/react-todolist/build/static/js/main.be46daa0.chunk.js"
+  },
+  {
+    "revision": "ab042855f15ae14d4bef",
+    "url": "/react-todolist/build/static/js/runtime-main.cbe05d35.js"
+  }
+]);
